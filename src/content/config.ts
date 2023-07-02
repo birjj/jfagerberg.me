39bfae4120310e246273f7180173efc74ffe5420
Johan Fagerberg <johanringmann@gmail.com>
2023-07-02 21:27:17 +0200

    feat: add blog posts as pages

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    tags: z.array(z.string()),
+    wip: z.boolean().default(false),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
