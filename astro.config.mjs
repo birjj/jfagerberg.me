4a81571868ce3743d3dff40270e0f8229269af08
Johan Fagerberg <johanringmann@gmail.com>
2023-08-08 18:21:25 +0200

    feat: add support for MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import { remarkHyphenate } from "./remark-plugins/remark-hyphenate.mjs";
 import remarkUnwrapImages from "remark-unwrap-images";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
 import expressiveCode from "astro-expressive-code";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,11 +15,15 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkHyphenate, remarkUnwrapImages],
   },
+  mdx: {
+    remarkPlugins: [remarkHyphenate, remarkUnwrapImages],
+  },
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     preact(),
     expressiveCode(),
+    mdx(),
   ],
 });
