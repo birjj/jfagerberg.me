7740571274b8cf72e13976ab2ccd29e76da0f1d4
Johan Fagerberg <johanringmann@gmail.com>
2023-08-09 14:13:46 +0200

    fix: use light mode unless user has actively chosen dark

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -6,7 +6,7 @@ import useDarkMode from "../hooks/use-darkmode";
 export type DarkModeToggleProps = JSX.IntrinsicElements["div"] & {};
 const DarkModeToggle = ({ ...props }: DarkModeToggleProps) => {
   const id = useId();
-  const { isDarkMode, toggle } = useDarkMode(true);
+  const { isDarkMode, toggle } = useDarkMode(false);
 
   useEffect(() => {
     document.documentElement.classList.toggle("dark", isDarkMode);
