0f9867307cb4c08f02071c80c9cbcc41deb5f24a
Johan Fagerberg <johanringmann@gmail.com>
2024-07-16 12:52:44 +0000

    feat: add change and publish date to blog posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,7 @@ const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     date: z.date(),
+    updated: z.date().optional(),
     tags: z.array(z.string()),
     draft: z.boolean().default(false),
   }),
